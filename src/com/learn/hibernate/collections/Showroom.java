0e85821a2ca6d46a0781462ee69cc0a7cdcfe8a5
subirrastogi <subirrastogi@gmail.com>
2014-10-06 06:38:56 +0530

    ff

--- src/com/learn/hibernate/collections/Showroom.java
+++ src/com/learn/hibernate/collections/Showroom.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
+
 @Entity
 @Table(name="ShOWROOM_LIST")
 public class Showroom {
